7cfb108ed4d8478f7428d9df5dd3ec8857e29776
priyo105 <adnankamal972@gmail.com>
2023-02-10 18:06:34 +0000

    reducers setup

---
+++ frontend_react/src/state/index.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// this data is accessable throughout the entire application
+const initialState={
+    mode:"light", //dark mode or light mode
+    user:null, 
+    token: null,
+    posts:[]
+}
+
+
+export const authSlice=createSlice({
+    name:"auth",
+    initialState,
+
+    reducers:{
+      setMode: (state)=>{
+        state.mode= state.mode === "light"?"dark":"light" //if light, then light other wise dark    
+      },
+
+      setLogin: (state,action)=>{
+        state.user=action.payload.user;
+        state.token=action.payload.token
+      },
+
+      setLogout:(state)=>{
+        state.user=null;
+        state.token=null;
+      },
+
+      setFriends: (state,action) =>{
+      
+        if(state.user){
+            state.user.friends = action.payload.friends
+        }else{
+            console.error("User Does not have any friends....")
+        }
+      },
+      setPosts:(state,action)=>{
+       state.posts=action.payload.posts;
+      },
+      setPost: (state,action)=>{
+        const updatedPosts= state.posts.map((post)=>{
+            if(post._id === action.payload.post_id) return action.payload.post;
+            return post;
+        })
+      }
+
+    }
+})
+
+export const {setMode,setLogin,setLogout,setFriends,setPosts, setPost}=authSlice.actions;
+export default  authSlice.reducer;
