1b3c9d116b78ea178a2acdee24b1cfc5e086a492
priyo105 <adnankamal972@gmail.com>
2023-03-27 05:54:51 +0100

    login success

---
+++ social_mobile/api/ApiUrl.js
@@ -0,0 +1,13 @@
+
+const ApiUrl= ()=>{
+
+ const BaseUrl="http://192.168.56.1:3001"
+
+ Login=BaseUrl+"/auth/login"
+
+
+
+
+}
+
+export default ApiUrl;
--- social_mobile/screens/Login.jsx
+++ social_mobile/screens/Login.jsx
@@ -7,154 +7,43 @@ import { useState } from "react";
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import React from 'react';
-import {Text, View, TextInput, Button, StyleSheet } from 'react-native';
+import axios from "axios";
+import {Text, View, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
-const loginHandle=()=>{
-
-
+const loginHandle=(values,navigation)=>{
+ console.log(values);
+ axios.post('http://192.168.56.1:3001/auth/login',values)
+ .then(response => {
+   console.log(response.data)
+   
+ })
+ .catch(error => {
+   console.log(error);
+ });
 
 }
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  password: Yup.string().min(3, 'Password must be at least 3 characters').required('Password is required'),
 });
 
 
-// const Login= ({navigation})=>{
 
-//   const handleFormSubmit = async (values, onSubmitProps) => {
-//        console.log('entered here');
-//        console.warn('entered here');
-//   };
-
-//    return(
-
-//     // <View >
-//     //       <Text style={styles.text}>PicTik</Text>
-//     //       <Text style={{textAlign:"center",marginTop:0,marginHorizontal:20, fontSize:30,fontWeight:"800",color:"navy"}}>Login Here </Text>
-//     //       <Text style={{textAlign:"center",marginTop:20,marginHorizontal:60, fontSize:18,fontWeight:"800",color:"black",fontFamily:'rubik'}}>Welcome Back, Your Friends are waiting for you to Join </Text>
-          
-
-//     //   {/* main part here */}
-//       <Formik
-//           initialValues={{ email: '', password: '' }}
-//           validationSchema={validationSchema}
-//           onSubmit={(values)=>console.log(values)}
-//     >
-//     {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-//         <View>
-//           <View style={{marginTop:30}}>
-//             <RoundedTextField 
-//                  placeholder={"Email"} 
-//                  onBlur={handleBlur('email')} 
-//                  value={values.email}
-//                  onChangeText={handleChange('email')}/>
-//                   {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
-//           </View>  
-
-//           <View style={{marginTop:30}}>
-//             <RoundedTextField 
-//                    placeholder={"Password"}  
-//                    onBlur={handleBlur('password')} 
-//                    value={values.password}
-//                    secureEntry={true}
-//                    onChangeText={handleChange('password')} />
-//           </View> 
-
-//                      <TextInput
-//             style={styles.input}
-//             placeholder="Email"
-//             onChangeText={handleChange('email')}
-// onBlur={handleBlur('email')}
-//             value={values.email}
-//           /> 
-
-//           <Text style={{textAlign:"right",marginTop:20,marginHorizontal:40, fontSize:13,fontWeight:"600",color:"navy"}}>Forgot Your Password ? </Text>
-//           <View style={{marginTop:70,alignContent:"center", alignSelf:"center"}}>
-//             {/* <RoundedButton title={"Sign In"} width={300} onPress={handleSubmit}></RoundedButton> */}
-//             <Button  onPress={handleSubmit} title="Submit" />
-
-//           </View> 
-//      {/* main part here */}
-//      </View>
-//     )} 
-//      </Formik>
-
-
-//           /* <Text style={{textAlign:"center",marginTop:20,marginHorizontal:60, fontSize:13,fontWeight:"800",color:"black",fontFamily:'rubik'}}>Create new account </Text>
-          
-//           <Text style={{textAlign:"center",marginTop:20,marginHorizontal:60, fontSize:13,fontWeight:"800",color:"navy",fontFamily:'rubik'}}>Or continue with </Text>
-        
-//         <View style={{flexDirection:"row",marginHorizontal:100,marginTop:30}}>
-//                 <Ionicons
-//                     testID="nextButton"
-//                     name="logo-google"
-//                     color="black"
-//                     padding={10}
-//                     backgroundColor="#d8e0ed"
-//                      size={24} />
-
-//                 <Ionicons
-//                     testID="nextButton"
-//                     name="logo-facebook"
-//                     color="black"
-//                     marginLeft={30}
-//                     padding={10}
-//                     backgroundColor="#d8e0ed"
-//                      size={24} />
-
-//                 <Ionicons
-//                     testID="nextButton"
-//                     name="logo-apple"
-//                     color="black"
-//                     marginLeft={30}
-//                     padding={10}
-//                     backgroundColor="#d8e0ed"
-//                      size={24} />
-
-
-//         </View> */
-         
-          
- 
- 
-//    )
-// }
-
-
-
-
-
-
-// const validationSchema = Yup.object().shape({
-//   name: Yup.string().required('Name is required'),
-//   email: Yup.string().email('Invalid email').required('Email is required'),
-//   password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-// });
-
-const Login = ({ handleSubmit }) => {
+const Login = ({ handleSubmit,navigation}) => {
   return (
 
 
     <Formik
-      initialValues={{ name: '', email: '', password: '' }}
+      initialValues={{email: '',password: '' }}
       validationSchema={validationSchema}
       onSubmit={(values)=>console.log(values)}
     >
       {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
         <View style={styles.container}>
 
- 
-{/* 
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            onChangeText={handleChange('email')}
-            onBlur={handleBlur('email')}
-            value={values.email}
-          /> */}
+
               <Text style={styles.text}>PicTik</Text>
              <Text style={{textAlign:"center",marginTop:0,marginHorizontal:20, fontSize:30,fontWeight:"800",color:"navy"}}>Login Here </Text>
              <Text style={{textAlign:"center",marginTop:20,marginHorizontal:60, fontSize:18,fontWeight:"800",color:"black",fontFamily:'rubik'}}>Welcome Back, Your Friends are waiting for you to Join </Text>
@@ -178,7 +67,7 @@ const Login = ({ handleSubmit }) => {
         {touched.password && errors.password && <Text style={styles.error}>{errors.password}</Text>}
          
          <View style={{marginTop:20}}>
-          <RoundedButton  title="Submit" onPress={handleSubmit} />
+          <RoundedButton  title="Submit" onPress={()=>{loginHandle(values,navigation)}} />
          </View> 
             <View>
                <Text style={{textAlign:"center",marginTop:20,marginHorizontal:60, fontSize:13,fontWeight:"800",color:"black",fontFamily:'rubik'}}>Create new account </Text>
