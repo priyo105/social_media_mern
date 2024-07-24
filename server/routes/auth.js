fb97afe009dc81474780a87b3c088aab808d6945
priyo105 <adnankamal972@gmail.com>
2024-07-24 05:50:42 +0100

    changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,6 @@ import {login} from "../controllers/auth.js"
 const router= express.Router();
 
 router.post("/login",login);
-router.post("/test",(req,res)=>{res.send('<h1> Hello world</h1>')});
+router.post("/test",(req,res)=>{res.send('Heyyyyy')});
 
 export default router;
